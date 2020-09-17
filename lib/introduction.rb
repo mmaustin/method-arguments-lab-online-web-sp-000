841b0f7d3effde25cc305e06360e1422a1d329d8
mccray austin <tubach75@hotmail.com>
2020-09-17 12:03:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,7 @@ def introduction(name)
 end
 introduction("Josh")
 
-def introduction_with_name(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_name("Dan", "Ember.js")
+introduction_with_language("Dan", "Ember.js")
